fa5f8bfba1b02e11996b22500ff712e60d98b11a
Miroslav <miskin@abv.bg>
2018-05-06 14:20:19 +0200

    Simple Conditions. Add second exam problem. TIme to full a pool (code improvements).

--- 2018_03_18_Simple_Conditions/18_Pool_pipes.py
+++ 2018_03_18_Simple_Conditions/18_Pool_pipes.py
@@ -6,10 +6,11 @@ h = float(input())
 litres_p1 = p1 * h
 litres_p2 = p2 * h
 
-litres_both = litres_p1 + litres_p2
-
-if litres_both <= v:
-    print("The pool is {0}% full. Pipe 1: {1}%. Pipe 2: {2}%."
-          .format(int((litres_both/v) * 100), int((litres_p1 * 100)/litres_both), int((litres_p2 * 100)/litres_both)))
+if (litres_p1 + litres_p2) <= v:
+    total = int(((litres_p1 + litres_p2)/v) * 100)
+    pipe1 = int((litres_p1/(litres_p1+litres_p2))*100)
+    pipe2 = int((litres_p2/(litres_p1+litres_p2))*100)
+    print("The pool is {0}% full. Pipe 1: {1}%. Pipe 2: {2}%.".format(total, pipe1, pipe2))
 else:
-    print("For {0} hours the pool overflows with {1} liters.".format(h, (litres_both-v)))
+    print("For {0} hours the pool overflows with {1} liters.".format(h, ((litres_p1+litres_p2)-v)))
+
