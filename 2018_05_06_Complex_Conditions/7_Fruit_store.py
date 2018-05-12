22048dce4ff655b2f961947a99ae45ed9d38fbf4
Miroslav <miskin@abv.bg>
2018-05-12 21:22:20 +0200

    Complex conditions. Add problem 7. Fruit store.

---
+++ 2018_05_06_Complex_Conditions/7_Fruit_store.py
@@ -0,0 +1,39 @@
+fruit = input().lower()
+day = input().lower()
+quantity = float(input())
+
+if day == 'monday' or day == 'tuesday' or day == 'wednesday' or day == 'thursday' or day == 'friday':
+    if fruit == 'banana':
+        price = 2.50
+    elif fruit == 'apple':
+        price = 1.20
+    elif fruit == 'orange':
+        price = 0.85
+    elif fruit == 'grapefruit':
+        price = 1.45
+    elif fruit == 'kiwi':
+        price = 2.70
+    elif fruit == 'pineapple':
+        price = 5.50
+    elif fruit == 'grapes':
+        price = 3.85
+elif day == 'saturday' or day == 'sunday':
+    if fruit == 'banana':
+        price = 2.70
+    elif fruit == 'apple':
+        price = 1.25
+    elif fruit == 'orange':
+        price = 0.90
+    elif fruit == 'grapefruit':
+        price = 1.60
+    elif fruit == 'kiwi':
+        price = 3.00
+    elif fruit == 'pineapple':
+        price = 5.60
+    elif fruit == 'grapes':
+        price = 4.20
+
+try:
+    print('{0:0.2f}'.format(price*quantity))
+except NameError:
+    print('Error')
